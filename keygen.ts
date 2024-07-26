399c874a2b2d4060a9a27df9d8e52cb9107e7c3b
xentoshi <xentoshi@protonmail.com>
2024-07-26 12:27:45 +0900

    turbin3-prereq challenge

---
+++ keygen.ts
@@ -0,0 +1,11 @@
+import { Keypair } from "@solana/web3.js";
+
+let kp = Keypair.generate();
+
+console.log(
+  `You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`
+);
+
+console.log(
+  `Secret key: ${kp.secretKey.toString()}`
+);
